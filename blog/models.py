623f1d79248ea24686d1242e468d6542710c25ac
sylpi <syl.pillet@hotmail.fr>
2020-06-13 23:19:27 +0200

    Initial commit

---
+++ blog/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+class PostCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    category = models.ForeignKey('PostCategory', null=True, blank=True, on_delete=models.DO_NOTHING)
+
+    published = models.BooleanField(default=True)
+    text = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    STATUS_VISIBLE = 'visible'
+    STATUS_HIDDEN = 'hidden'
+    STATUS_MODERATED = 'moderated'
+
+    STATUS_CHOICES = (
+        (STATUS_VISIBLE, 'Visible'),
+        (STATUS_HIDDEN, 'Hidden'),
+        (STATUS_MODERATED, 'Moderated')
+    )
+
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
+    author_name = models.CharField(max_length=100)
+    text = models.TextField()
+    status = models.CharField(max_length=20, default=STATUS_VISIBLE, choices=STATUS_CHOICES, editable=True)
+    moderation_text = models.CharField(max_length=200, blank=True, editable=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '{} - {} (status : {}'.format(self.author_name, self.text[:20], self.status)
